c4f3fd2180b51c3b17ac012944cf400d2be6eb17
threaciorr <126417778+threaciorr@users.noreply.github.com>
2023-08-14 16:47:57 +0200

    javascript + czyszczenie kodu teraz wezme sie za mediaQuery

---
+++ script.js
@@ -0,0 +1,11 @@
+const newsletterMessageShow = document.querySelector('.newsletter__container');
+const newsletterMessage = document.querySelector('.newsletter-message');
+const newsletterMessageClose = document.querySelector('.newsletter-message__close');
+
+newsletterMessageShow.addEventListener('click', () => {
+    newsletterMessage.classList.add('active__newsletter-message');
+});
+
+newsletterMessageClose.addEventListener('click', () => {
+    newsletterMessage.classList.remove('active__newsletter-message');
+});
